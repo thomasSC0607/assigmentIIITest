9dec9c5c9de1085353c2f179933d39d71512ba59
Thomas <thomasbu2004@gmail.com>
2024-05-10 20:52:24 -0500

    Commit en rama Main

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public void imprimirNombre() {
+        System.out.println("Thomas Buitrago Uribe");
+    }
+    public static void main(String[] args) {
+
+        Main m = new Main();
+        m.imprimirNombre();
+    }
+}
